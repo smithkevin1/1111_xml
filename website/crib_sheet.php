92503556f917beb8f9468de8c92dcf6d5c22ff4d
smithkevin1 <kgsmith2@gmail.com>
2016-08-29 18:52:38 -0400

    Add and clean up crib sheet for 1111.

---
+++ website/crib_sheet.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="header-container">
+            <header class="wrapper clearfix">
+                <h1 class="title">Elements for AWD 3302</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Course Site</a></li>
+                        <li><a href="http://fa16.kevingeraldsmith.com/index.php">Project Page</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+              <article>
+                    <header>
+                      <h2>General elements:</h2>
+                    </header>
+                    <section>
+                      <h3>&lt;ref&gt;</h3>
+                      <p>
+                        (reference) defines a reference to another location; can contain any URI
+                      </p>
+                    </section>
+
+                    <section>
+                      <h3>&lt;note&gt;</h3>
+                      <p>
+                        (note) contains a note made on the text intended to provide additional
+                information or comment upon the text; requires a @type attribute and a @resp
+                (responsible party) attribute with your id for peer reviewer note
+                      </p>
+                    </section>
+                    <section>
+                      <h3>&lt;q&gt;</h3>
+                      <p>(quote) quoted text.</p>
+                    </section>
+
+
+              </article> <aside> <h3>Element List</h3> <p>This is a list of XML
+              elements for ENGW1111. This is not an exhaustive list, but should
+              provide a nice reference to consult while composing. This page
+              will be updated as additional genre models are produced in
+              projects 2 and 3.</p> </aside> </div> <!-- #main --> </div> <!--
+              #main-container -->
+
+        <div class="footer-container">
+            <footer class="wrapper">
+                <h3></h3>
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
