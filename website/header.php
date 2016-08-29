1637b1735a6a742fbae97b520ca3e93cfb6537c2
smithkevin1 <kgsmith2@gmail.com>
2016-08-29 12:01:21 -0400

    Add initial site files (CSS, JS, pHp)

---
+++ website/header.php
@@ -0,0 +1,11 @@
+<div class="header-container">
+    <header class="wrapper clearfix">
+        <h1 class="title">AWD 3302: Project Files</h1>
+        <nav>
+            <ul>
+                <li><a href="http://su16awd.kevingeraldsmith.com">Course Site</a></li>
+                <li><a href="http://su16awd.kevingeraldsmith.com/projects/crib_sheet.php">Element List</a></li>
+            </ul>
+        </nav>
+    </header>
+</div>
